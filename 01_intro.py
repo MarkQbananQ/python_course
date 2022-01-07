6bb956ec2e749181bc05b36f2e5996c3c45a6cea
MarkQbananQ <msuvorov2005@gmail.com>
2022-01-07 14:21:35 +0100

    initial commit

---
+++ 01_intro.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+# komentarz
+
+# %%
+print(2 + 2)
+
+# %%
+print(3 * 3)
+# %%
+print(3 + 2 * 2)
+print(4 - 2 * 2)
+# %%
+print(10 // 3)
+print(7 // 6)
+# %%
+print(2 ** 5)
+# %%
