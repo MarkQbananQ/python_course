ce1990cd2593d3aa3314951e269d180f357458a6
MarkQbananQ <msuvorov2005@gmail.com>
2022-01-07 14:43:21 +0100

    01_intro

--- 01_intro.py
+++ 01_intro.py
@@ -16,3 +16,28 @@ print(7 // 6)
 # %%
 print(2 ** 5)
 # %%
+print(10 % 3)
+print(12 % 5)
+print(10 % 2)
+print(10 % 7)
+# %%
+print( (10 - 2 ** 3) * 10 )
+# %%
+a = 10
+b = 20
+# %%
+c = a * b
+print(c)
+# %%
+print("Love Python")
+# %%
+print("It's the best!")
+# %%
+print('It\'s the best!')
+# %%
+print('Python 3.10')
+# %%
+print('Python\n3.10')
+# %%
+print("""Python
+3.10""")
