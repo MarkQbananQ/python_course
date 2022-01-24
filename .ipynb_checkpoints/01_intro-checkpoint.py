77c1130a003ef99b32b8d85cfe6b8c1bebf617a2
MarkQbananQ <msuvorov2005@gmail.com>
2022-01-24 14:38:18 +0100

    first commit 01.24

---
+++ .ipynb_checkpoints/01_intro-checkpoint.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+
+# komentarz
+
+# %%
+print(2 + 2)
+
+# %%
+print(3 * 3)
+# %%
+print(3 + 2 * 2)
+print(4 - 2 * 2)
+# %%
+print(10 // 3)
+print(7 // 6)
+# %%
+print(2 ** 5)
+# %%
+print(10 % 3)
+print(12 % 5)
+print(10 % 2)
+print(10 % 7)
+# %%
+print( (10 - 2 ** 3) * 10 )
+# %%
+a = 10
+b = 20
+# %%
+c = a * b
+print(c)
+# %%
+print("Love Python")
+# %%
+print("It's the best!")
+# %%
+print('It\'s the best!')
+# %%
+print('Python 3.10')
+# %%
+print('Python\n3.10')
+# %%
+print("""Python
+3.10""")
+# %%
+print('\tPython')
+print('\t\t\tPython')
+# %%
+print('C:\path\\to\something\\new')
+print(r'C:\path\to\something\new')
+# %%
+import os
+os.getcwd()
+# %%
+print("""
+Instukcja uruchamiania pliku przyklad.py:
+
+    --file [nazwa pliku]
+        zapisuje output do pliku
+    
+    --quiet
+        wycisza logi z konsoli
+
+Koniec.""")
+# %%
+text = 'I love Python! '
+print(text * 3)
+print('hau...' * 8)
+print('*' * 30)
+# %%
+name = 'Python'
+print(name, '3.10')
+# %%
+age = 16
+imie = 'Marek'
+
+print(imie, 'ma', age, 'lat')
+print('{} ma {} lat'.format(imie, age))
+print('{0} ma {1} lat'.format(imie, age))
+# %%
