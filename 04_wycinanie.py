ba6f537e07ca66524e2811812b34ae324d8c26fe
MarkQbananQ <msuvorov2005@gmail.com>
2022-01-24 20:03:26 +0100

    04 wycinanie finished

--- 04_wycinanie.py
+++ 04_wycinanie.py
@@ -3,3 +3,27 @@ name = 'Python'
 # %%
 # print(name[0])
 print(name[-2])
+# %%
+# name[start:stop]
+# name[:stop]
+name[0:4]
+name[:4]
+name[2:]
+
+name[:]
+# %%
+name[-3:]
+name[-3:-1]
+# %%
+full = 'Python Programming'
+print(full[7:])
+print(full[::2])
+# %%
+sample = '#stop#this#flow'
+print(sample[::5])
+# %%
+numbers = '8,9,7,4'
+print(numbers[::2])
+# %%
+name = 'kajak'
+print(name[::-2])
