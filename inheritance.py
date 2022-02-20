dd572c429bab0967882b840ab147d8a738499a8b
MarkQbananQ <msuvorov2005@gmail.com>
2022-02-20 21:02:07 +0100

    inheritance

---
+++ inheritance.py
@@ -0,0 +1,13 @@
+class Person:
+    def __init__(self, fname, lname):
+        self.firstname = fname
+        self.lastname = lname
+    
+    def print_name(self):
+        print(self.firstname, self.lastname)
+    
+
+class Student(Person):
+    def __init__(self, fname, lname):
+        super().__init__(fname, lname)
+        self.graduation_year = 2019
